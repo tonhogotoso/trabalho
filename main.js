9fda04f6218be78fccbba6f5f270bec56479ff10
aaaaaaaaa <112512930+tonhogotoso@users.noreply.github.com>
2024-05-09 21:13:59 -0300

    bb

--- main.js
+++ main.js
@@ -15,10 +15,10 @@ for (let i = 0; i < botoes.length; i++) {
 }
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2024-10-05T00:00:00");
-const tempoObjetivo2 = new Date("2024-12-05T00:00:00");
-const tempoObjetivo3 = new Date("2024-12-30T00:00:00");
-const tempoObjetivo4 = new Date("2025-02-01T00:00:00");
+const tempoObjetivo1 = new Date("2024-10-06T00:00:00");
+const tempoObjetivo2 = new Date("2027-1-06T00:00:00");
+const tempoObjetivo3 = new Date("2027-2-100T00:00:00");
+const tempoObjetivo4 = new Date("2030-28-07T00:00:00");
 
 const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
 
